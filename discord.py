03f1854cae8f6dd8f40006bf66da89e0d18c06f2
Devon <DevonHurteau@gmail.com>
2024-09-19 10:45:17 -0600

    Added discord file

---
+++ discord.py
@@ -0,0 +1,17 @@
+from discordwebhook import Discord
+
+class DiscordBot:
+    def __init__(self, webhook_url):
+        self.discord = Discord(url=webhook_url)
+        self.discord_color = int('3498db', 16)
+
+    def discord_message(self, new_line_amount, league):
+        message = f"{new_line_amount} New Line Added For {league}" if new_line_amount == 1 else f"{new_line_amount} New Lines Added For {league}"
+
+        embed = {
+            "title": f"{league} Notification",
+            "description": message,
+            "color": self.discord_color,
+        }
+
+        self.discord.post(embeds=[embed])
